baff3ead9ae5c72bd0e42b9fc37f5bc1ddaf45ee
rthakurr <riyatwebdeveloper@gmail.com>
2024-04-02 17:22:57 +0530

    My first Commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+  
+@section('contents')
+  <div class="row">
+    Dashboard
+  </div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright © reserved with Code4Each</span>
+    </div>
+  </div>
+</footer>
