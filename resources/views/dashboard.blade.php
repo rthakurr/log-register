2b76954367b611475490e47766a317d59b75f71f
rthakurr <165756852+rthakurr@users.noreply.github.com>
2024-04-03 12:23:28 +0530

    Update dashboard.blade.php to show the count of product on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.app')
 
-  
 @section('contents')
-  <div class="row">
- Welcome 
-  </div>
+<div class="row">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                Dashboard Statistics
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h5 class="card-title">Number of Products: {{ $productCount }}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Product list content here -->
 @endsection
